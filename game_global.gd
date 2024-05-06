5096d237a4b1fc0434b0f843ca7f29ef8d4b008d
unknown <tiago@trattecinformatica.com.br>
2024-05-06 11:40:29 -0300

    ADD - Audio Moedas

--- game_global.gd
+++ game_global.gd
@@ -1,3 +1,4 @@
 extends Node
 
+var valor_moeda_amarela = 10
 var moedas = 0
--- moeda.gd
+++ moeda.gd
@@ -1,12 +1,10 @@
 extends Area2D
 
-var moeda:int = 1
-
-
 
 func _on_body_entered(body):
 	if (body.name == "Jogador"):
 		# GameGlobal.moedas = GameGlobal.moedas + 1
-		GameGlobal.moedas += 1		
+		GameGlobal.moedas += GameGlobal.valor_moeda_amarela
 		print("Coletou Moeda: "+str(GameGlobal.moedas))
-		queue_free()
+		$AudioStreamPlayer2D.play()
+		#queue_free()
