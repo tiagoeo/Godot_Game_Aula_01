3a7715e270a02aa3c379e4b787091b2aac4fdf58
Tiago <tiago@trattecinformatica.com.br>
2024-05-21 19:20:31 -0300

    MOD Aguardar termino audio moedas

--- moeda.gd
+++ moeda.gd
@@ -3,8 +3,12 @@ extends Area2D
 
 func _on_body_entered(body):
 	if (body.name == "Jogador"):
+		# - Variáverl Global moedas
 		# GameGlobal.moedas = GameGlobal.moedas + 1
 		GameGlobal.moedas += GameGlobal.valor_moeda_amarela
 		print("Coletou Moeda: "+str(GameGlobal.moedas))
+		
+		# - Audio
 		$AudioStreamPlayer2D.play()
-		#queue_free()
+		await $AudioStreamPlayer2D.finished
+		queue_free()
